bc67fabd843afdc4a337198a293e430d781d9c6c
rsancho64 <rsancho@iesrioarba.es>
2023-10-23 12:11:03 +0200

    .

--- comunic.con.global.c
+++ comunic.con.global.c
@@ -12,15 +12,14 @@ int aumenta(void)
 
 int disminuye(void)
 {
-    global--;
-    return 0;
+    return global - 1;
 }
 
 int main(int)
 {
     printf("main0: %d\n", global);
-    global = aumenta();  // ya es una funcion
+    global = aumenta();   // ya es una funcion
     printf("main0: %d\n", global);    
-    disminuye();         // todavia es un procedimiemto
+    global = disminuye(); // ya es una funcion
     printf("mainF: %d\n", global);
 }
